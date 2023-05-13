5f8d9f0c050b13a06eaea39b78fbc017a823efbc
Ohm <ohm22496@gmail.com>
2023-05-13 14:19:49 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Flex, Spacer,Text } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,7 +33,10 @@ export default function Home() {
           </a>
         </div>
       </div>
-
+      <Flex w="500px" bg="red">
+        <Text>adasdasdasdasdasdasdaasdasd</Text>
+      </Flex>
+      
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
